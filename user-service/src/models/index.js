aaef6ed509e070dafd3dd39515c137535cae260b
cw21 <chriswu.ca@gmail.com>
2024-07-12 14:39:24 -0700

    add dockerfile

--- user-service/src/models/index.js
+++ user-service/src/models/index.js
@@ -1,7 +1,7 @@
 const sequelize = require('../../../common/database');
 const User = require('./user');
 
-sequelize.sync({ force: true })
+sequelize.sync({ force: true }) // change true later to avoid data loss each restart
     .then(() => {
         console.log('Database & tables created!');
     });
