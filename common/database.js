a615b0359e8fdb27213389acf0ada96170490931
cw21 <chriswu.ca@gmail.com>
2024-07-19 14:35:47 -0700

    docker-compose

--- common/database.js
+++ common/database.js
@@ -2,9 +2,8 @@ const { Sequelize } = require('sequelize');
 const config = require('../user-service/config/config.json').development;
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
+    host: config.host, // This should now be 'db'
     dialect: config.dialect
 });
 
 module.exports = sequelize;
-// test
