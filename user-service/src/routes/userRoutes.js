9e271e8a99e502f464183b2a900ef197807e14c6
cw21 <chriswu.ca@gmail.com>
2024-07-19 14:40:39 -0700

    added healthcheck endpoint

--- user-service/src/routes/userRoutes.js
+++ user-service/src/routes/userRoutes.js
@@ -5,4 +5,7 @@ const router = express.Router();
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 
+// healthcheck endpoint
+router.get('/health', (req, res) => res.status(200).send('OK'));
+
 module.exports = router;
